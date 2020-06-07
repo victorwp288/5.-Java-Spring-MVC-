d7b4dc2fae5754ff065fcf0781f27e2b77c6dc38
victor <victorwpetersen@gmail.com>
2020-06-07 08:14:01 +0200

    laptop commit

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,11 +1,22 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.Cat;
+import com.example.demo.Service.CatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
-    public String index(){
+    @Autowired
+    CatService catService;
+
+    public String index(Model model){
+        List<Cat> cats = catService.getAllCats();
+        model.addAttribute("cats", cats);
         return "index";
     }
 
--- src/main/java/com/example/demo/Service/CatService.java
+++ src/main/java/com/example/demo/Service/CatService.java
@@ -1,12 +1,29 @@
 package com.example.demo.Service;
 
+import com.example.demo.Model.Cat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CatService {
 
-    public void findCatById(int catId){
+    List cats = new ArrayList<>();
 
+    public CatService() {
+        cats.add(new Cat("Tom", "Blå", 12, 9, 1));
+        cats.add(new Cat("Bob", "Sort", 1, 6, 2));
+        cats.add(new Cat("Lars", "Prikket", 5, 7, 3));
+        cats.add(new Cat("Thomas", "Stribet", 14, 1, 4));
+        cats.add(new Cat("Kim", "Hvid", 2, 4, 5));
+        cats.add(new Cat("Pelse", "Brun", 9, 3, 6));
+        cats.add(new Cat("Torben", "Rød", 4, 2, 7));
+    }
+
+    public void findCatById(int catId){
+        return ;
     }
 
     public void deleteCat(int catId){
@@ -25,7 +42,8 @@ public class CatService {
 
     }
 
-    public void getAllCats(){
+    public List<Cat> getAllCats(){
+        return cats;
 
     }
 }
